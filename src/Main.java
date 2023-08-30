a5c83627b2af2ac929c0b07cbfd9fc88eb8fdebe
Riccardo Gicalone <jack7riki@gmail.com>
2023-08-30 15:01:09 +0200

    project updated with same implementations

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 /**
  * Contenuto: Programma Gestione lista spesa.
  *
- * @author Riccardo Giacalone, Emanuele Conte, Devy Cantalupo.
+ * @author Riccardo Giacalone, Emanuele Conte.
  */
 public class Main {
     public static void main(String[] args) {
@@ -19,10 +19,16 @@ public class Main {
         while (!exit) {
             // Mostra le opzioni del menu
             System.out.println("Seleziona un'opzione:");
-            System.out.println("1. Aggiungi prodotto");
-            System.out.println("2. Rimuovi prodotto");
-            System.out.println("3. Visualizza lista prodotti");
-            System.out.println("4. Esci");
+            System.out.println("1. Aggiungi prodotto.");
+            System.out.println("2. Rimuovi prodotto.");
+            System.out.println("3. Visualizza lista prodotti.");
+            System.out.println("4. Segna prodotto come completato.");
+            System.out.println("5. Imposta valuta principale.");
+            System.out.println("6. Salva su file.");
+            System.out.println("7. Esci.");
+
+            double totalExpanse = productList.totalCalculationExpanse();
+            System.out.println("Totale spesa stimato: €" + totalExpanse);
             
             int choice = scanner.nextInt(); // Legge l'input dell'utente come intero
             scanner.nextLine(); // Consuma il carattere newline dopo la lettura dell'intero
@@ -54,6 +60,24 @@ public class Main {
                 }
                 case 3 -> productList.displayProductList();
                 case 4 -> {
+                    System.out.println("Inserisci il nome del prodotto da segnare come completato:");
+                    String productToComplete = scanner.nextLine();
+                    productList.markProductAsCompleted(productToComplete);
+                }
+                case 5 -> {
+                    System.out.println("Inserisci il simbolo della valuta desiderata:");
+                    String newCurrencySymbol = scanner.nextLine();
+                    productList.setCurrencySymbol(newCurrencySymbol);
+                    System.out.println("Simbolo della valuta impostato su " + newCurrencySymbol);
+                    break;
+                }
+                case 6-> {
+                    System.out.println("Inserisci il nome del file in cui salvare la lista:");
+                    String fileName = scanner.nextLine();
+                    productList.saveToFile(fileName);
+                    break;
+                }
+                case 7 -> {
                     exit = true;
                     System.out.println("Grazie per aver utilizzato il programma. Arrivederci!");
                 }
