d64a6a5c0d29901f376cda010a19ab086fd9c8ec
Emanuele <conte.emanuele@protonmail.com>
2023-09-11 12:17:21 +0200

    eliminato doppio metodo isCompleted

--- src/Product.java
+++ src/Product.java
@@ -15,37 +15,32 @@ public class Product {
         this.name = name;
         this.completed = false;
     }
-    
+
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
-    
+
     public void setPrice(float price) {
         this.price = price;
     }
-    
+
     public void setCompleted() {
         completed = true;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public int getQuantity() {
         return quantity;
     }
-    
+
     public float getPrice() {
         return price;
     }
 
-    public boolean isCompleted(){return completed;}
-        }
-
-    
     public boolean isCompleted() {
         return completed;
     }
 }
-
